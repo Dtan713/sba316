16be66c11e71d4d383d0aad339ed4ec972bd910e
Dtan713 <s.donaldtan@gmail.com>
2024-05-30 12:32:52 -0400

    some of the work

--- sba316.js
+++ sba316.js
@@ -1,18 +1,23 @@
-const don = document.selectELementById("don");
+// const don = document.selectELementById("don");
 
-const won = document.querySelector("won");
+// const won = document.querySelector("won");
 
-const firstChild = document.getElementsByName("first-Child");
+// const firstChild = document.getElementsByName("first-Child");
 
-const firstChildElement = firstChild.parentNode;
+// const firstChildElement = firstChild.parentNode;
 
-const elements = ["don", "won", "firstChild" ];
-    for (let i = 0; i < elements.length; i++) {
+// const elements = ["don", "won", "firstChild" ];
+//     for (let i = 0; i < elements.length; i++) {
 
-const sun = document.createElement("sun");
-mainEl.appendChild(sun);
+// const sun = document.createElement("sun");
+// mainEl.appendChild(sun);
+
+// let i = document.getElementById("sun");
+// let = cloneNode(sun);
 
 
 
- }
+//  }
+ 
+
  
