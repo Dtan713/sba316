49b7e28f5711aa737b17af80a068eabbf900ffd9
Dtan713 <s.donaldtan@gmail.com>
2024-05-30 10:49:35 -0400

    sixth task

--- sba316.js
+++ sba316.js
@@ -1,14 +1,16 @@
 const don = document.selectELementById("don");
+
 const won = document.querySelector("won");
+
 const firstChild = document.getElementsByName("first-Child");
+
 const firstChildElement = firstChild.parentNode;
 
 const elements = ["don", "won", "firstChild" ];
     for (let i = 0; i < elements.length; i++) {
-    // console.log(element[i]);
 
 const sun = document.createElement("sun");
-// mainEl.append(sun);
+mainEl.appendChild(sun);
 
 
 
