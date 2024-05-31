b2d9c4aea6a001f4f346073e22cf3317ea5a989c
Dtan713 <s.donaldtan@gmail.com>
2024-05-31 12:14:12 -0400

    adding more of DOM

--- sba316.js
+++ sba316.js
@@ -2,13 +2,21 @@ const form = document.getElementById ("form");
 console.log (form);
 
 const firstName = document.getElementById ("fname");
-const lastName = document.getElementById ("lname"); 
+const lastName = document.getElementById ("lname");
+const OrderNumber = document.getElementById ("Order-Number") 
 const Email = document.getElementById ("Email");
-
+const message = document.getElementById ("message");
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
     console.log(firstName.value);
     console.log(lastName.value);
+    console.log(OrderNumber.value);
     console.log(Email.value);
-})
+    console.log(message); 
+    let h1 = document.createElement ("h1");
+    h1.innerText = `Thank you, ${firstName.value} ${lastName.value} ${OrderNumber.value} ${Email.value} ${message}
+    message.appendChild (h1);
+
+    // alert(`Thank you ${firstName.value}`)
+}
